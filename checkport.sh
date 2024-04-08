2226896d49f3a279629e64f3b1b03f7aec11b83c
woshichenghaibo <80568737+woshichenghaibo@users.noreply.github.com>
2024-04-08 19:21:20 +0800

    Update checkport.sh

--- checkport.sh
+++ checkport.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
-
-# 获取当前正在监听5244端口的应用程序信息
-listening_apps=$(netstat -tuln | grep 5244 | grep -v "LISTEN")
-
-# 判断是否有应用程序在监听5244端口
-if [ -z "$listening_apps" ]; then
-    echo "没有应用程序正在监听5244端口。"
-else
-    echo "以下应用程序正在监听5244端口："
+    # Get the application information currently listening on port 5244
+    listening_apps=$(netstat -tuln | grep 5244 | grep -v "LISTEN")
+    # Determine if there is an application listening on port 5244
+    if [ -z "$listening_apps" ]; then
+    echo "No application is listening on port 5244."
+            # Wechat message push:
+            curl --location --request POST 'https://api.anpush.com/push/ZUM0GJE81YZJGOEWOQZ0TWTXAT1JFD' \
+                  --header 'Content-Type: application/x-www-form-urlencoded' \
+                  --data-urlencode 'title=Port Err:' \
+                  --data-urlencode 'content=## Alist Port 5244 Error... ' \
+                  --data-urlencode 'channel=18965'
+            # Wechat message push.
+    else
+    echo "The following application is listening on port 5244:"
     echo "$listening_apps"
-fi
+    fi
+    
